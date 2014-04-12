23b55af1b263ff932af6ec9279ade5ee7585ead1
HRTGroup <admin@hackerromteam.de>
2014-04-12 14:40:12 +0200

    Create übersetzung_1_extendedcommands.c

---
+++ übersetzung_1_extendedcommands.c
@@ -0,0 +1,134 @@
+Installing:
+Installation aborted.
+Checking if loki-fying is needed
+Install from sdcard complete.
+Install update from zip file
+Choose zip from
+Choose zip from
+Choose zip Using Free Browse Mode
+Choose zip from Last Install Folder
+Install zip from sideload
+Install Multiple zip Files
+Setup Free Browse Mode
+Signature Verification
+Verify zip md5sum
+Couldn't open directory
+Failed to close directory.
+No files found.
+Can't mount
+Choose a zip to apply
+Yes - Install
+Confirm install?
+Can't mount
+Choose an image to restore
+Confirm restore?
+Yes - Restore
+Choose a backup to delete
+Yes - Delete
+Confirm delete?
+USB Mass Storage device
+Leaving this menu unmounts
+your SD card from your PC.
+Unmount
+THIS CAN NOT BE UNDONE.
+No
+unknown volume
+can't format_volume
+format_volume failed to unmount
+format_volume: format_rfs_device failed on
+format_volume failed to unmount
+format_volume: no MTD partition
+format_volume: can't open MTD
+format_volume: can't erase MTD
+format_volume: can't close MTD
+format_volume: make_ext4fs failed on
+format_volume: mkfs.f2f2 failed on
+Formatting unknown device.
+No app2sd partition found. Skipping format of /sd-ext.
+Formatting ext3 device.
+Error while unmounting
+Formatting ext2 device.
+Error mounting
+Skipping format...
+Mounts and Storage Menu
+Confirm format?
+Yes - Format
+mount
+unmount
+format
+mount USB storage
+Formatting /data...
+Error formatting
+Done.
+Error unmounting
+Error mounting
+Formatting
+Error formatting
+Done
+Default Backup Format
+Default backup format set to tar
+Default backup format set to dedupe
+Default backup format set to tar + gzip
+Backup to
+Restore from
+Delete from
+Custom Backup to
+Custom Restore from
+Backup and Restore
+Clone ROM to update.zip
+Free Unused Backup Data
+Misc Nandroid Settings
+Unsupported in open source version!
+TWRP backup format must be tar(.gz)!
+Format device
+Confirm formatting?
+Yes - Format device
+Could not format
+Done formatting
+Can't partition device
+Ext Size
+Swap Size
+Partition Type
+Partitioning SD Card... please wait...
+An error occured while partitioning your SD Card. Please see /tmp/recovery.log for more details.
+Can't format unknown volume:
+Can't partition non-vfat:
+Can't partition non mmcblk device:
+Can't partition unsafe device:
+Advanced power options
+Reboot Recovery
+Reboot to Bootloader
+Power Off
+Reboot to Download Mode
+Rebooting recovery...
+Rebooting to
+Shutting down...
+Advanced Menu
+Wipe Dalvik Cache
+Report Error
+Key Test
+Show log
+Partition
+Apply Loki Patch
+Confirm wipe?
+Yes - Wipe Dalvik Cache
+Outputting key codes.
+Go back to end debugging.
+Unable to get recovery.fstab info for %s during fstab generation!
+Unable to create
+Completed outputting fstab.
+Unable process volume! Skipping...
+could not mount
+could not unmount
+Checking for ext4 partitions...
+Filesystems need to be converted to ext4.
+A backup and restore will now take place.
+If anything goes wrong, your backup will be
+named %s. Try restoring it
+in case of error.
+ROM may flash stock recovery on boot. Fix?", "Yes - Disable recovery flash"
+Root access possibly lost. Fix?", "Yes - Fix root (/system/bin/su)
+Root access possibly lost. Fix?", "Yes - Fix root (/system/xbin/su)
+Root access is missing. Root device?", "Yes - Root device (/system/xbin/su)
+
+
